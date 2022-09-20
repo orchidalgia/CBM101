d681c2775d13d31b783d8ac20545d242596acba9
orchidalgia <alekkok@uef.fi>
2022-09-20 15:14:21 +0300

    update supervised learning

--- F_Machine_learning/2_Supervised-Learning/solutions/pyc_2.py
+++ F_Machine_learning/2_Supervised-Learning/solutions/pyc_2.py
@@ -1,2 +1,11 @@
 
-px.histogram(df, x='thal', color=target, facet_row_spacing=1).show()
+g = sns.pairplot(df.loc[:, ['thal', 'oldpeak', 'maximum_heart_rate_achieved', 'number_of_major_vessels', 'Disease']], 
+                 diag_kind='kde', 
+                 hue='Disease', 
+                 markers=["o", "s"])
+
+# None of the pairwise comparisons can separate the classes 
+# but there seems to be some correlation between these variables and disease.
+# Correlation is easier to see in continuous variables (max heart rate & oldpeak separates the groups pretty well)
+# In descrete variables it's harder to see but seems that number of major vessels correlates with disease
+# 
