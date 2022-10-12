d8a0e97e2c2ad8fc60b395f7db81fdefbe6746b8
orchidalgia <alekkok@uef.fi>
2022-10-12 15:02:02 +0300

    update data_resources nbs

--- C_Data_resources/solutions/ex2_1.py
+++ C_Data_resources/solutions/ex2_1.py
@@ -1,4 +1,12 @@
-def plot(k):
-    plt.imshow(X[k].reshape(8,8), cmap='gray')
-    plt.title(f"Number = {y[k]}")
-    plt.show()
+
+# new column for classes
+df['species'] = data.target
+
+
+# to get the target_names:
+n = data.target_names 
+
+# you can use map-function ('replace' works too)
+df['species'] = df.species.map({0:n[0], 
+                                1:n[1], 
+                                2:n[2]})
--- C_Data_resources/solutions/ex2_2.py
+++ C_Data_resources/solutions/ex2_2.py
@@ -1,20 +1,3 @@
 
-# example solution. 
-# You don't need to make a plotting function/loop,
-# you can simply call plt.imshow a number of times and observe
-
-print(faces.DESCR) # this shows there are 40 classes, 10 samples per class
-print(faces.target) #the targets i.e. classes
-print(np.unique(faces.target).shape) # another way to see n_classes
-
-X = faces.images
-y = faces.target # represents the test subjects in the picture
-
-fig = plt.figure(figsize=(16,5))
-idxs = [0,1,2, 11,12,13, 40,41]
-for i,k in enumerate(idxs):
-    ax=fig.add_subplot(2,4,i+1)
-    ax.imshow(X[k])
-    ax.set_title(f"target={y[k]}")
-    
-# looking at a few plots shows that each target is a single person.
+#The dates are not in order so we should've sorted them first:
+sydney = sydney.sort_values(by=['Date'])
